ee2a4f2d1afb35ec94ebc5da91823c1e9d746497
savvyaquib <itsmohammadaquib@gmail.com>
2026-05-01 06:44:42 +0530

    add dashboard controller

---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose"
+import { Video } from "../models/video.model.js"
+import { Subscription } from "../models/subscription.model.js"
+import { Like } from "../models/like.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const getChannelStats = asyncHandler(async (req, res) => {
+    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+    const totalViews = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "video",
+                as: "likes"
+            }
+        },
+        {
+            $group: {
+                _id: req.user._id,
+                totalViews: { $sum: "$views" },
+                totalVideos: { $sum: 1 },
+                totalSubscribers: { $sum: { $size: "$subscribers" } },
+                totalLikes: { $sum: { $size: "$likes" } }
+            }
+        }
+    ])
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, totalViews, "Channel stats fetched successfully"))
+
+})
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+    // TODO: Get all the videos uploaded by the channel
+})
+
+export {
+    getChannelStats,
+    getChannelVideos
+}
