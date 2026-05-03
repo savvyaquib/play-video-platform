5f93e192cc601b17fed2c0d61f3c9529a5dec139
savvyaquib <itsmohammadaquib@gmail.com>
2026-05-03 09:28:20 +0530

    add playlist controller and route

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import likeRouter from "./routes/like.route.js"
 import commentRouter from "./routes/comment.route.js"
 import tweetRouter from "./routes/tweet.route.js"
 import dashboardRouter from "./routes/dashboard.route.js"
+import playlistRouter from "./routes/playlist.route.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter)
@@ -31,5 +32,6 @@ app.use("/api/v1/dashboard", dashboardRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/playlists", playlistRouter)
 
 export { app }
